5cf7ef6ab7ff799da8f630b3df46a1bbff25cd9a
Igor <marauman@gmail.com>
2018-06-27 13:11:45 -0600

    Confetti effect on projects
    Author:
    http://www.cagrimmett.com/til/2018/01/05/css-confetti.html

--- static/js/functions.js
+++ static/js/functions.js
@@ -230,3 +230,49 @@ function initMap(){
 
    
 }
+ //confetti functions
+    //https://codepen.io/anon/pen/JMOQzE
+function create(i) {
+    var width = Math.random() * 8;
+    var height = width * 0.5;
+    var colourIdx = Math.ceil(Math.random() * 3);
+    var colour = "red";
+    switch(colourIdx) {
+      case 1:
+        colour = "yellow";
+        break;
+      case 2:
+        colour = "blue";
+        break;
+      default:
+        colour = "red";
+    }
+    $('<div id="confettiID" class="confetti-'+i+' '+colour+'"></div>').css({
+      "width" : width+"px",
+      "height" : height+"px",
+      "top" : -Math.random()*20+"%",
+      "left" : Math.random()*100+"%",
+      "opacity" : Math.random()+0.5,
+      "transform" : "rotate("+Math.random()*360+"deg)"
+    }).appendTo('.confettiBlock');  
+    
+    drop(i);
+  }
+  
+  function drop(x) {
+    $('.confetti-'+x).animate({
+      top: "100%",
+    //   left: "+="+Math.random()*120+"%"
+    }, Math.random()*3000 + 5000, function() {
+      reset(x);
+    });
+  }
+  
+  function reset(x) {
+    $('.confetti-'+x).animate({
+      "top" : -Math.random()*20+"%",
+     //"left" : "-="+Math.random()*15+"%"
+    }, 0, function() {
+      drop(x);             
+    });
+  }
--- static/js/main.js
+++ static/js/main.js
@@ -1,43 +1,80 @@
 $(document).ready(function (e) {
-    var flag = true; // need this flag to run bar animation;
-    // animation for icons
-    $("i").hover(
-        function () {
-            $(this).addClass('animated swing');
-        },
-        function () {
-            $(this).removeClass("animated swing");
-        });
+  var flag = true; // need this flag to run bar animation;
+  // animation for icons
+  $("i").hover(
+    function () {
+      $(this).addClass('animated swing');
+    },
+    function () {
+      $(this).removeClass("animated swing");
+    });
 
 
-    $(window).scroll(function (evt) {
 
-        evt.preventDefault();
-        //get current position of the scroll
-        var currentScrlPos = $(window).scrollTop();
-        //get the top position of div
-        var scrollOffset = $('#mywork').offset().top;
-        //if user reaches the top of the page reset flag 
-        if (currentScrlPos >= 0 && currentScrlPos <= 50) {
-            flag = true;
+
+  $(".project").hover(
+
+    function (evt) {
+      //var bar = $(this).siblings();
+      var bar = $(this).find("*");
+      var progress = bar[3].getAttribute('data-progress').slice(0,-1);
+
+       if (progress == 100) {
+
+        $(this).addClass("confettiBlock");
+
+        for (var i = 0; i < 170; i++) {
+          create(i);
         }
-        //console.log(currentScrlPos);
-        if (flag) {
-            if (currentScrlPos >= scrollOffset) {
-                var bar = document.getElementsByClassName('progress-bar');
-                var width = 1;
+      }
+    },
+    function () {
+      var confetti = document.querySelectorAll('#confettiID');
 
-                bar = fillProgressBar(bar, width);
+      for (var i = 0; i < confetti.length; i++) {
+        confetti[i].remove();
+      }
+      $(this).removeClass('confettiBlock');
+    });
 
-                setTimeout(function () { emptyProgressBar(bar, width); }, 1000);
 
-                setTimeout(function () { finalProgressBar(bar); }, 2500);
-                flag = false;
-            }
-        }
+  $(window).scroll(function (evt) {
 
-    }); // window scroll
-    //google maps 
-    google.maps.event.addDomListener(window,'load', initMap);
+    evt.preventDefault();
+    //get current position of the scroll
+    var currentScrlPos = $(window).scrollTop();
+    //get the top position of div
+    var scrollOffset = $('#mywork').offset().top;
+    //if user reaches the top of the page reset flag 
+    if (currentScrlPos >= 0 && currentScrlPos <= 50) {
+      flag = true;
+    }
+    //console.log(currentScrlPos);
+    if (flag) {
+      if (currentScrlPos >= scrollOffset) {
+        var bar = document.getElementsByClassName('progress-bar');
+        var width = 1;
+
+        bar = fillProgressBar(bar, width);
+
+        setTimeout(function () { emptyProgressBar(bar, width); }, 1000);
+
+        setTimeout(function () { finalProgressBar(bar); }, 2500);
+        flag = false;
+      }
+    }
+
+  }); // window scroll
+  //google maps 
+  google.maps.event.addDomListener(window, 'load', initMap);
+
+
+
+
+
+
+
+
+
 
 });// \document
